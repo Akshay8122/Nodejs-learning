21cb7a7629dd52839d48b8d3954eab4669598eee
akshay.p <akshay.p@simformsolutions.com>
2024-10-15 17:37:56 +0530

    feat: implement url module in node.js

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
 let PORT = 5000;
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http
   .createServer((req, res) => {
+    if (req.url === "/favicon.ico") {
+      return res.end();
+    }
     const log = `${Date.now()} ${req.url} Request recived\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
 
     fs.appendFile("log.txt", log, (err, data) => {
-      switch (req.url) {
+      switch (myUrl.pathname) {
         case "/":
           res.end("HomePage");
           break;
         case "/about":
-          res.end("Hello from Akshay Patel");
+          const userName = myUrl.query.name;
+          res.end(`Hello, ${userName}`);
           break;
+
+        case "/search":
+          const serchResult = myUrl.query.search_query;
+          res.end("Here are your result for" + serchResult);
         default:
           res.end("404 Not Found");
       }
