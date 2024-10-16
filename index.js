576d4ff1c410962e2bcbeb17b54d84f4ef4d280e
akshay.p <akshay.p@simformsolutions.com>
2024-10-16 14:24:18 +0530

    feat : server setup with express

--- index.js
+++ index.js
@@ -1,42 +1,15 @@
 let PORT = 5000;
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+const express = require("express");
+const app = express();
 
-const myServer = http
-  .createServer((req, res) => {
-    if (req.url === "/favicon.ico") {
-      return res.end();
-    }
-    const log = `${Date.now()} ${req.url} Request recived\n`;
-    const myUrl = url.parse(req.url, true);
+app.get("/", (req, res) => {
+  return res.send(`Hello from home page`);
+});
 
-    fs.appendFile("log.txt", log, (err, data) => {
-      switch (myUrl.pathname) {
-        case "/":
-          if (req.method === "GET") res.end("HomePage");
-          break;
-        case "/about":
-          const userName = myUrl.query.name;
-          res.end(`Hello, ${userName}`);
-          break;
+app.get("/about", (req, res) => {
+  return res.send(
+    `Hey ${req.query.name}` + ` \n your id is ${req.query.userId}`
+  );
+});
 
-        case "/search":
-          const serchResult = myUrl.query.search_query;
-          res.end("Here are your result for" + serchResult);
-
-        case "/signup":
-          if (req.method === "GET") res.end("Welcome to signup page");
-          else if (req.method === "POST") {
-            // DB query
-
-            res.end("Suceess");
-          }
-        default:
-          res.end("404 Not Found");
-      }
-    });
-
-    console.log(req.headers);
-  })
-  .listen(PORT, () => console.log(`server listening on ${PORT}`));
+app.listen(PORT, () => console.log("server started"));
